0a424285fbea3497c4d1cec1b076bb786dad6764
Sarthak Gupta <sarthakgupta072@gmail.com>
2024-02-02 20:10:49 +0530

    feat: flex

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { Button, StyleSheet, Text, View } from "react-native"
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
 
 export default function App() {
   return (
     // View is used as a parent container component to hold the child components
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <Button title='Press me' onPress={() => alert("Button pressed")} />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='your course goal' style={styles.textInput} />
+        <Button title='Add Goal' />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 60,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "gray",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 })
