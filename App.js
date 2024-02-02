0a8dd985bc43368fbe867d5b394e7c9a7ca19857
Sarthak Gupta <sarthakgupta072@gmail.com>
2024-02-02 18:53:06 +0530

    feat: minor changes

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native"
 
 export default function App() {
   return (
+    // View is used as a parent container component to hold the child components
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello, World!</Text>
+      <Button title='Press me' onPress={() => alert("Button pressed")} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
