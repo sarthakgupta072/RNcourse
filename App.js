4a3e581df365015ad439f6b58d8acb908a60ab9c
Sarthak Gupta <sarthakgupta072@gmail.com>
2024-02-03 13:15:43 +0530

    feat: flatlist scrolling

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+} from "react-native"
+import { useState } from "react"
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("")
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredtext) => {
+    setEnteredGoalText(enteredtext)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, key: Math.random().toString() },
+    ])
+  }
+
   return (
     // View is used as a parent container component to hold the child components
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='your course goal' style={styles.textInput} />
-        <Button title='Add Goal' />
+        <TextInput
+          placeholder='your course goal'
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => (
+            <View style={styles.goalItem}>
+              <Text style={styles.goalText}>{itemData.item.text}</Text>
+            </View>
+          )}
+          keyExtractor={(itemData) => itemData.index}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   )
@@ -39,5 +76,15 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+    overflow: "scroll",
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
   },
 })
